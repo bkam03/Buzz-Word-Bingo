511fcafe1cb181ce86960d34c17a62078f18430f
BK <bkam03@yahoo.com>
2017-07-13 21:02:27 -1000

    cleaned up code

--- app.js
+++ app.js
@@ -2,23 +2,11 @@ const express = require( 'express' );
 const app = express();
 const bodyParser = require( 'body-parser' );
 
-
-/*
-  post will look like {
-    buzzWord : String,
-    points: Number,
-    heard: false
-  }
-*/
-
 function indexBuzzWordInArray( { buzzWord } ){ //return -1 if not found, else return position of word
   let indexMatchingWord = -1;
-  console.log( `buzzWord ${ buzzWord }` );
   //could use every or some array method for this
-  for( var i = 0; i < arrayofWords.length; i++ ){
-    let currentComparisonWord = arrayofWords[ i ].buzzWord;
-    console.log( currentComparisonWord );
-    if( currentComparisonWord === buzzWord ){
+  for( let i = 0; i < arrayofWords.length; i++ ){
+    if( arrayofWords[ i ].buzzWord === buzzWord ){
       indexMatchingWord = i;
       break;
     }
@@ -26,29 +14,32 @@ function indexBuzzWordInArray( { buzzWord } ){ //return -1 if not found, else re
   return indexMatchingWord;
 }
 
-var arrayofWords = [
-  {buzzWord:"c",points:"3",heard:"false"},{buzzWord:"2",points:"3",heard:"false"}  //delete this later
-];
-var userScore = 10;  //change to 0 later
+var arrayofWords = [];
+var userScore = 0;
 
 app.use( express.static( 'public' ) );
-
-
 app.use( bodyParser.json() );
+
 
 app.get( '/', ( req, res ) => {
   res.sendFile( '/index.html' );
 } );
+
 
 app.route( '/buzzword' )
   .get( ( req, res ) => {
     res.json( { buzzWords: arrayofWords } );
   } )
   .post( ( req, res ) => {
-    let buzzword = req.body;
-    let isSuccessful = indexBuzzWordInArray( buzzword ) === -1;
-    if( isSuccessful ){
-      arrayofWords.push( buzzword );
+    let isSuccessful = false;
+    if(  arrayofWords.length < 5 ){
+      let buzzword = req.body;
+      isSuccessful = indexBuzzWordInArray( buzzword ) === -1;
+      if( isSuccessful ){
+        arrayofWords.push( buzzword );
+      }
+    } else {
+      console.log( 'too many words' );
     }
     res.send( { success: isSuccessful } );
   } )
@@ -57,37 +48,30 @@ app.route( '/buzzword' )
     let isSuccessful = false;
     let index = indexBuzzWordInArray( req.body );
     let unusedWord = arrayofWords[ index ].heard === 'false';
-    console.log( `index ${ index } unusedWord ${ unusedWord }`);
     if( index > -1  && unusedWord ){
       arrayofWords[ index ].heard = heard;
       userScore += parseFloat( arrayofWords[ index ].points );
-      console.log( userScore );
       isSuccessful = true;
     }
     res.send( { success: isSuccessful } );
   } )
   .delete( ( req, res ) => {
     let index = indexBuzzWordInArray( req.body );
-
     let isWordInArray = index > -1;
     if( isWordInArray ){
       arrayofWords.splice( index, 1 );
     }
-
      res.send( { success: isWordInArray } );
   } );
 
 
-app.post( '/reset', ( req, res ) => { //body { 'reset': true }, resp. { 'success, etc'  resets server.  all buzzwords removed and scores = 0.}
+app.post( '/reset', ( req, res ) => {
   arrayofWords = [];
   userScore = 0;
-  console.log( arrayofWords, userScore );
   res.send( { success: true } );
 } );
 
 
-
-
 const server = app.listen( 3000, function(){
-  console.log( 'server running' );
+  console.log( 'server listening at 3000' );
 } );
